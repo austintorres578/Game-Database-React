064b1e5683c531d45062f1354614fbd4a957a852
Austin Torres <austintorres578@gmail.com>
2026-03-27 23:27:45 -0400

    Delete src/src directory

--- src/src/utils/gamePage/libraryDocId.js
+++
@@ -1,8 +0,0 @@
-/**
- * Converts a RAWG game ID to the string format used as the Firestore
- * library document ID. Returns null if no ID is provided.
- */
-export function getLibraryDocId(rawgId) {
-  if (rawgId === null || rawgId === undefined) return null;
-  return String(rawgId);
-}
